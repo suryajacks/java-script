3662b8f8babafdf3d72014e738113d0c76412f5d
suryajacks <suryajacks16@gmail.com>
2023-01-08 12:49:35 +0530

    javascript

---
+++ one _node.js
@@ -0,0 +1,56 @@
+// number divisible by 7
+
+
+var a = 25;
+if(a%7 === 0)
+{
+console.log("divisible by 7")
+}
+else{
+    console.log("not divisible by 7")
+}
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
